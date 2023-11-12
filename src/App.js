aaf76d7f2b82398c36218fef1772da72b4860937
willokans <Slide2014!>
2023-11-12 12:20:45 +0000

    Add table to home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
