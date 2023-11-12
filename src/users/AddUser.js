de2a74e196c7266bfd69db5933ac9770e494c0d9
willokans <Slide2014!>
2023-11-12 20:47:03 +0000

    Create Register User Form using React

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,5 +1,54 @@
 import React from "react";
 
 export default function AddUser() {
-  return <div>AddUser Form</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your name"
+              name="name"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="Username" className="form-label">
+              Username
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your username"
+              name="username"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type={"email"}
+              className="form-control"
+              placeholder="Enter your email"
+              name="email"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <button type="submit" className="btn btn-outline-danger mx-2">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
